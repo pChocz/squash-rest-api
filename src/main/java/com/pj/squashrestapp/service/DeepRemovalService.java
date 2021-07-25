4ee9a4231f687e1f7964c0c5fc224cce4ebc3eaa
pChocz <piotr@choczynski.pl>
2021-07-25 18:12:02 +0200

    league remove comments added

--- src/main/java/com/pj/squashrestapp/service/DeepRemovalService.java
+++ src/main/java/com/pj/squashrestapp/service/DeepRemovalService.java
@@ -20,7 +20,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-/** */
+/**
+ * Service for removal of whole league implementing bulk deletion, which drastically limits number
+ * of SQL queries and improves performance.
+ *
+ * <p>NOTE: removal of matches from {@link MatchRepository} still takes way too much time. To be
+ * checked later if it's possible to improve.
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -31,7 +37,6 @@ public class DeepRemovalService {
   // additional match
   private final AdditionalSetResultRepository additonalSetResultRepository;
   private final AdditionalMatchRepository additionalMatchRepository;
-
   // round match
   private final SetResultRepository setResultRepository;
   private final MatchRepository matchRepository;
@@ -39,39 +44,40 @@ public class DeepRemovalService {
   private final RoundRepository roundRepository;
   private final BonusPointRepository bonusPointRepository;
   private final SeasonRepository seasonRepository;
-
   // trophies
   private final TrophiesForLeagueRepository trophiesForLeagueRepository;
-
   // league
   private final LeagueRepository leagueRepository;
 
   @Transactional
   public void deepRemoveLeague(final UUID leagueUuid) {
-
-    // NOTE: order matters here, it needs to remove entities from the bottom up.
-    final List<BulkDeletableByLeagueUuid> repos =
-        List.of(
-            // additional matches
-            additonalSetResultRepository,
-            additionalMatchRepository,
-            // bonus points and trophies
-            bonusPointRepository,
-            trophiesForLeagueRepository,
-            // round matches
-            setResultRepository,
-            matchRepository,
-            roundGroupRepository,
-            roundRepository,
-            seasonRepository);
-
-    repos.forEach(repository -> deleteAllByLeagueUuid(repository, leagueUuid));
-
-    // league
+    getAllRequiredRepos().forEach(repository -> deleteAllByLeagueUuid(repository, leagueUuid));
     final League league = leagueRepository.findByUuidRaw(leagueUuid);
     leagueRepository.delete(league);
   }
 
+  /**
+   * NOTE: order matters here, it needs to remove entities from the bottom up.
+   *
+   * @return list of all repos for which bulk deletion should be applied
+   */
+  private List<BulkDeletableByLeagueUuid> getAllRequiredRepos() {
+    return List.of(
+        additonalSetResultRepository,
+        additionalMatchRepository,
+        bonusPointRepository,
+        trophiesForLeagueRepository,
+        setResultRepository,
+        matchRepository,
+        roundGroupRepository,
+        roundRepository,
+        seasonRepository);
+  }
+
+  /**
+   * Performs bulk-deletion of items. Partitioning is applied as number of IDs passed to
+   * delete-method is limited, otherwise StackOverflow exception would occur.
+   */
   private void deleteAllByLeagueUuid(
       final BulkDeletableByLeagueUuid repository, final UUID leagueUuid) {
     final List<Long> ids = repository.fetchIdsByLeagueUuidRaw(leagueUuid);
