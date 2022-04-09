fcbfc0f524cd33ea9649c0985ed9f7c0c18d7d93
Piotr Choczyński <piotr@choczynski.pl>
2022-04-09 15:47:10 +0200

    added matches count for round scoreboard (to show progress bar)

--- src/main/java/com/pj/squashrestapp/dto/scoreboard/RoundScoreboard.java
+++ src/main/java/com/pj/squashrestapp/dto/scoreboard/RoundScoreboard.java
@@ -12,77 +12,84 @@ import com.pj.squashrestapp.model.RoundGroup;
 import com.pj.squashrestapp.util.GeneralUtil;
 import com.pj.squashrestapp.util.MatchExtractorUtil;
 import com.pj.squashrestapp.util.RomanUtil;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 /** */
 @Getter
 @NoArgsConstructor
 public class RoundScoreboard {
 
-  private String leagueName;
-  private UUID leagueUuid;
+    private String leagueName;
+    private UUID leagueUuid;
 
-  private UUID seasonUuid;
-  private int seasonNumber;
-  private String seasonNumberRoman;
-  private String split;
+    private UUID seasonUuid;
+    private int seasonNumber;
+    private String seasonNumberRoman;
+    private String split;
 
-  private UUID roundUuid;
-  private int roundNumber;
+    private UUID roundUuid;
+    private int roundNumber;
 
-  @JsonSerialize(using = LocalDateSerializer.class)
-  @JsonDeserialize(using = LocalDateDeserializer.class)
-  @JsonFormat(pattern = GeneralUtil.DATE_FORMAT)
-  private LocalDate roundDate;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(pattern = GeneralUtil.DATE_FORMAT)
+    private LocalDate roundDate;
 
-  @JsonIgnore private List<Integer> playersPerGroup;
-  private List<RoundGroupScoreboard> roundGroupScoreboards;
-  private boolean finishedState;
+    @JsonIgnore
+    private List<Integer> playersPerGroup;
 
-  public RoundScoreboard(final Round round) {
-    this.leagueName = round.getSeason().getLeague().getName();
-    this.leagueUuid = round.getSeason().getLeague().getUuid();
+    private List<RoundGroupScoreboard> roundGroupScoreboards;
+    private boolean finishedState;
 
-    this.seasonUuid = round.getSeason().getUuid();
-    this.seasonNumber = round.getSeason().getNumber();
-    this.seasonNumberRoman = RomanUtil.toRoman(this.seasonNumber);
-    this.split = round.getSplit();
+    private int numberOfAllMatches;
+    private int numberOfFinishedMatches;
 
-    this.roundUuid = round.getUuid();
-    this.roundNumber = round.getNumber();
-    this.roundDate = round.getDate();
-
-    this.roundGroupScoreboards = new ArrayList<>();
-    this.playersPerGroup = new ArrayList<>();
-    this.finishedState = round.isFinished();
-  }
-
-  public void addRoundGroupNew(final RoundGroup roundGroup) {
-    final List<MatchDetailedDto> matches = MatchExtractorUtil.extractAllMatches(roundGroup);
-    final RoundGroupScoreboard scoreboard = new RoundGroupScoreboard(matches);
-    roundGroupScoreboards.add(scoreboard);
-    playersPerGroup.add(scoreboard.getScoreboardRows().size());
-  }
-
-  public void assignPointsAndPlaces(final List<Integer> xpPoints) {
-    int i = 0;
-    for (final RoundGroupScoreboard scoreboard : roundGroupScoreboards) {
-      int j = 1;
-      for (final RoundGroupScoreboardRow scoreboardRow : scoreboard.getScoreboardRows()) {
-        scoreboardRow.setPlaceInRound(i + 1);
-        scoreboardRow.setPlaceInGroup(j++);
-        scoreboardRow.setXpEarned(xpPoints.get(i++));
-      }
+    public RoundScoreboard(final Round round) {
+        this.leagueName = round.getSeason().getLeague().getName();
+        this.leagueUuid = round.getSeason().getLeague().getUuid();
+        this.seasonUuid = round.getSeason().getUuid();
+        this.seasonNumber = round.getSeason().getNumber();
+        this.seasonNumberRoman = RomanUtil.toRoman(this.seasonNumber);
+        this.split = round.getSplit();
+        this.roundUuid = round.getUuid();
+        this.roundNumber = round.getNumber();
+        this.roundDate = round.getDate();
+        this.roundGroupScoreboards = new ArrayList<>();
+        this.playersPerGroup = new ArrayList<>();
+        this.finishedState = round.isFinished();
     }
-  }
 
-  @Override
-  public String toString() {
-    return "Round Scoreboard - R: " + roundNumber + " | S: " + seasonNumber + " | " + leagueName;
-  }
+    public void addRoundGroupNew(final RoundGroup roundGroup) {
+        final List<MatchDetailedDto> matches = MatchExtractorUtil.extractAllMatches(roundGroup);
+        final RoundGroupScoreboard scoreboard = new RoundGroupScoreboard(matches);
+        roundGroupScoreboards.add(scoreboard);
+        playersPerGroup.add(scoreboard.getScoreboardRows().size());
+    }
+
+    public void assignPointsAndPlaces(final List<Integer> xpPoints) {
+        int i = 0;
+        for (final RoundGroupScoreboard scoreboard : roundGroupScoreboards) {
+            int j = 1;
+            for (final RoundGroupScoreboardRow scoreboardRow : scoreboard.getScoreboardRows()) {
+                scoreboardRow.setPlaceInRound(i + 1);
+                scoreboardRow.setPlaceInGroup(j++);
+                scoreboardRow.setXpEarned(xpPoints.get(i++));
+            }
+            this.numberOfAllMatches += scoreboard.getMatches().size();
+            this.numberOfFinishedMatches += scoreboard.getMatches().stream()
+                    .filter(MatchDetailedDto::checkFinished)
+                    .count();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Round Scoreboard - R: " + roundNumber + " | S: " + seasonNumber + " | " + leagueName;
+    }
 }
